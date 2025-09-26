088229bc0a324e1fcb84bbf801f60636ec11f3d3
Marco Wettstein <maw@panter.ch>
2025-09-26 21:48:59 +0200

    bump

--- apps/www/payload.config.ts
+++ apps/www/payload.config.ts
@@ -1,5 +1,6 @@
 import { buildConfig } from "payload";
-import { postgresAdapter } from "@payloadcms/db-postgres";
+import { vercelPostgresAdapter } from "@payloadcms/db-vercel-postgres";
+
 import { lexicalEditor } from "@payloadcms/richtext-lexical";
 import { vercelBlobStorage } from "@payloadcms/storage-vercel-blob";
 import path from "path";
@@ -159,7 +160,7 @@ export default buildConfig({
   typescript: {
     outputFile: path.resolve(dirname, "payload-types.ts"),
   },
-  db: postgresAdapter({
+  db: vercelPostgresAdapter({
     pool: {
       connectionString: process.env.DATABASE_URI || "",
     },
