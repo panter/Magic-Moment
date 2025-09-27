92ed03e5719a3f8bbb6c1d4ee561921a69293e51
Marco Wettstein <maw@panter.ch>
2025-09-27 14:24:15 +0200

    bump

--- apps/www/src/lib/sharp.ts
+++ apps/www/src/lib/sharp.ts
@@ -1,4 +1,4 @@
 export const getSharp = async () => {
-  // see https://github.com/vercel/vercel/issues/14001
-  return import("@img/sharp-wasm32").then((sharp) => sharp.default);
+  // see
+  return import("sharp").then((sharp) => sharp.default);
 };
