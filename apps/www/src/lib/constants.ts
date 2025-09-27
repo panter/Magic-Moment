5f642ce3d5ecd96458336676118067b947996c4c
Marco Wettstein <maw@panter.ch>
2025-09-27 12:31:37 +0200

    fix: fix

--- apps/www/src/lib/constants.ts
+++ apps/www/src/lib/constants.ts
@@ -1,5 +1,8 @@
 export const MESSAGE_CHAR_LIMIT = 200;
 
-export const NEXT_PUBLIC_URL = process.env.NEXT_PUBLIC_VERCEL_URL
+export const NEXT_PUBLIC_URL = process.env
+  .NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
+  : process.env.NEXT_PUBLIC_VERCEL_URL
   ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
   : `http://${process.env.NEXT_PUBLIC_LOCAL_URL}`;
